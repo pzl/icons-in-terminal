d34af16091ef234d18182661fecc0120eaed47bb
pzl <dan@panzarel.la>
2017-07-27 12:00:08 -0400

    add C array script to build step

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ mkdir -p ./build
 ./scripts/inte_without_codepoint.sh > ./build/icons_bash_without_codepoint.sh
 ./scripts/inte_emacs.sh > ./build/icons-in-terminal.el
 ./scripts/inte_c_header.sh > ./build/icons-in-terminal.h
+./scripts/inte_c_arr.sh > ./build/icons-in-terminal.c
 chmod +x ./build/icons.fish
 chmod +x ./build/icons_bash.sh
 mv icons-in-terminal.ttf ./build/
